0fe81ec79eb0fab8a9c942e76a6241545c66b254
Simon Martinelli <simon@72.services>
2023-01-18 10:30:35 +0100

    Organized imports

--- backend/src/main/java/ch/martinelli/demo/backend/person/PersonController.java
+++ backend/src/main/java/ch/martinelli/demo/backend/person/PersonController.java
@@ -1,7 +1,6 @@
 package ch.martinelli.demo.backend.person;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
